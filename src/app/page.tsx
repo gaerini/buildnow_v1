dbdd33504c2c18b3a26d5988c3421659c962479f
Do-Woung-Kong <kdwoong0612@naver.com>
2024-02-02 20:35:20 +0900

    dw svg webpack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
+import Icon from "../../common/components/Icon/Icon";
 
 export default function Home() {
-  return <></>;
+  return <Icon name="PencilIcon" width={50} height={50} />;
 }
