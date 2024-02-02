01703f2c284c42905d2e45fcd512e75eaf2161ef
kyle9909 <kyle9909@naver.com>
2024-02-03 04:10:42 +0900

    ks checkbox 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@
 import Image from "next/image";
 import Icon from "../../common/components/Icon/Icon";
 import Dropdown from "../../common/components/Dropdown/Dropdown";
+import CheckBox from "../../common/components/CheckBox/CheckBox";
 
 export default function Home() {
   return (
     <>
+      <CheckBox text1="한국" text2="호주" />
       <Dropdown />
     </>
   );
