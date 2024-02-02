75c3522f8e68ed603d4c59d200562dd2c2acb57d
Do-Woung-Kong <kdwoong0612@naver.com>
2024-02-03 02:59:17 +0900

    dw topnavigator 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,17 @@
 
 import Image from "next/image";
 import SideNavigator from "../../common/components/SideNavigator/SideNavigator";
+import TopNavigator from "../../common/components/TopNavigator/TopNavigator";
 
 export default function Home() {
-  return <SideNavigator CompanyName="A 건설" />;
+  return (
+    <div className="flex h-screen overflow-x-hidden">
+      <div className="fixed top-0 left-0 h-full z-10">
+        <SideNavigator CompanyName="A 건설" />
+      </div>
+      <div className="w-full">
+        <TopNavigator />
+      </div>
+    </div>
+  );
 }
