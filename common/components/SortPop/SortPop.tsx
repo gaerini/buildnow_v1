17f3996e957024bd9c10f8a523ced36aa09f2a21
Do-Woung-Kong <kdwoong0612@naver.com>
2024-02-10 00:56:00 +0900

    dw table SortingPopup 완료 (2/3)

--- common/components/SortPop/SortPop.tsx
+++ common/components/SortPop/SortPop.tsx
@@ -7,60 +7,57 @@ interface SortOption {
   icon: string;
 }
 
-interface item {
-  name: string;
-  column: string | null;
-  sort?: keyof ScoreSummary; // sort 프로퍼티의 타입을 keyof CompanyData로 지정
-  class: string;
-  wclass?: string;
-  pclass?: string;
-  icon?: boolean;
-}
-
 interface Props {
-  item: item;
+  column: string | null;
   currentOptions: SortOption[];
-  onOptionClick: (column: string | null) => void;
+  onOptionClick: (column: string | null, isAscending: boolean) => void;
   modalPosition: { x: number; y: number };
   modalRef: RefObject<HTMLDivElement>;
 }
 
 const Modal: React.FC<Props> = ({
-  item,
+  column,
   currentOptions,
   onOptionClick,
   modalPosition,
   modalRef,
-}) => (
-  <div
-    ref={modalRef}
-    style={{
-      position: "absolute",
-      left: `${modalPosition.x}px`,
-      top: `${modalPosition.y}px`,
-      backgroundColor: "white",
-      boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-      borderRadius: "8px",
-      padding: "10px",
-      zIndex: 1000, // 모달창이 다른 요소들 위에 표시되도록 z-index 설정
-    }}
-  >
-    {/* 모달창 내용 */}
-    {currentOptions.map((option, index) => (
-      <div
-        key={index}
-        className="modal-option"
-        onClick={() => onOptionClick(item.column)}
-      >
-        <div className="flex-col justify-center items-center">
-          <div className="px-4 py-1 inline-flex justify-center items-center gap-2">
-            <Icon name={option.icon} width={16} height={16} />
-            <p>{option.label}</p>
+}) => {
+  // 정렬 옵션 클릭 이벤트 처리
+  const handleOptionClick = (option: SortOption) => {
+    // 옵션에 따라 정렬 방향 결정 (높은 순: 오름차순, 낮은 순: 내림차순)
+    const isAscending = option.label === "높은 순";
+    // 선택된 정렬 기준과 방향을 ScoreTable로 전달
+    onOptionClick(column, isAscending);
+  };
+
+  return (
+    <div
+      ref={modalRef}
+      className="bgColor-white py-2 border-[1px] rounded-m borderColor"
+      style={{
+        position: "absolute",
+        left: `${modalPosition.x}px`,
+        top: `${modalPosition.y}px`,
+        boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+        zIndex: 1000, // 모달창이 다른 요소들 위에 표시되도록 z-index 설정
+      }}
+    >
+      {/* 모달창 내용 */}
+      {currentOptions.map((option, index) => (
+        <div key={index}>
+          <div className="justify-center items-center">
+            <button
+              className="w-[122px] px-4 py-1 whitespace-nowrap inline-flex gap-2 items-center justify-start hover:bgColor-blue hover:textColor-focus "
+              onClick={() => handleOptionClick(option)}
+            >
+              <Icon name={option.icon} width={16} height={16} />
+              <p>{option.label}</p>
+            </button>
           </div>
         </div>
-      </div>
-    ))}
-  </div>
-);
+      ))}
+    </div>
+  );
+};
 
 export default Modal;
--- common/components/TopNavigator/TopNavigator.tsx
+++ common/components/TopNavigator/TopNavigator.tsx
@@ -2,14 +2,11 @@ import React, { ReactNode, useEffect } from "react";
 
 interface TopNavigatorProps {
   children: ReactNode;
-
 }
 
-const TopNavigator: React.FC<TopNavigatorProps> = ({
-  children,
-}) => {
+const TopNavigator: React.FC<TopNavigatorProps> = ({ children }) => {
   return (
-    <div className="sticky top-0 h-16 bgColor-white border-b border justify-start items-center inline-flex">
+    <div className="sticky top-0 h-16 bgColor-white border-b justify-start items-center inline-flex z-5">
       <div className="flex items-center ml-8 w-full grow">{children}</div>
     </div>
   );
