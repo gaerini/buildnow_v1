7a13c58e7c4ee90454a2ec86aba9d66e25fdce2a
Do-Woung-Kong <kdwoong0612@naver.com>
2024-04-19 00:47:23 +0900

    dw 0418 recruiter 미달 정렬 오류 개선

--- common/components/ExcelDownload/ExcelDownload.tsx
+++ common/components/ExcelDownload/ExcelDownload.tsx
@@ -5,7 +5,7 @@ const downloadExcel = (data: ApplierListData[]) => {
   const worksheet = XLSX.utils.json_to_sheet(data);
   const workbook = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(workbook, worksheet, "my_sheet");
-  XLSX.writeFile(workbook, "신영씨앤디_0320_협력사서류심사결과.xlsx");
+  XLSX.writeFile(workbook, "한울건설_협력사서류심사결과.xlsx");
 };
 
 export default downloadExcel;
